cf0e6f0172b61126ca78e05f7f600f13b8d5c1ae
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-04-14 14:40:48 +0600

    button style done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,27 +3,44 @@ import { Button, Form } from 'react-bootstrap';
 import './Login.css'
 
 const Login = () => {
+    const handleEmailBlur = (event) => {
+        console.log(event.target.value)
+    }
+    const handlePasswordBlur = (event) => {
+        console.log(event.target.value)
+    }
+    const handleFormSbmit = event => {
+        console.log('from submit')
+        event.preventDefault();
+    }
     return (
         <div>
-            <h4>Please Login</h4>
-            <Form className='login-part'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    
-                </Form.Group>
+            <div className='registration w-25 mx-auto'>
+                <h4 className='text-primary '>Please Login</h4>
+                <div className='d-flex justify-content-between'>
+                    <Button>Google SignIn</Button>
+                    <Button>Facebook</Button>
+                    <Button>Github</Button>
+                </div>
+                <Form onSubmit={handleFormSbmit} className='input-form'>
+                    <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 };
--- src/Components/Regester/Regester.js
+++ src/Components/Regester/Regester.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Regester = () => {
     return (
         <div>
-            <h4>Plaease Regester</h4>
+            <div className='registration w-25 mx-auto'>
+                <h4 className='text-primary'>Please Regester First</h4>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Your Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Your Name" required />
+
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" required />
+
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="Regester">
+                        Regester
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 };
